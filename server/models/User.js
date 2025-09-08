014d77a78372278b9806c077bc428413e31d3e4c
velizhask <velizhasandy@gmail.com>
2025-09-08 19:35:34 +0700

    feat: implement comprehensive server enhancements and email system

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   email: {
     type: String,
